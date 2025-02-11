856361f99f0092ea01a2efc8347696b53bb8a548
Mario <you@example.com>
2025-02-11 14:02:44 +0300

    test

--- src/formsend.php
+++ src/formsend.php
@@ -4,27 +4,24 @@ $phone = $_POST['phone'];
 $chat_name = "@MarkAndNastya";
 $token = "7471342210:AAEDkhuLXZootfnjOjDWpbKoeNLSuxzJhUw";
 $message = "Привет. \nУ нас заявочка с сайта расчет пенсии.\nИмя клиента - $name\n телефон - $phone\n Всем хорошего дня!";
-
+echo"$message";
 $text = urlencode($message);
 //$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id=sfrlaw&text={$text}";
 
 $url = "https://api.telegram.org/bot7471342210:AAEDkhuLXZootfnjOjDWpbKoeNLSuxzJhUw/sendMessage?chat_id=markandNastya&text=test";
 
 $ch = curl_init();
-$optArray = array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true
-);
+$optArray = [CURLOPT_URL => $url, CURLOPT_RETURNTRANSFER => true];
 curl_setopt_array($ch, $optArray);
 $result = curl_exec($ch);
 
 curl_close($ch);
 
-
+/*
 if (isset($_POST['name'])) {
     ?>
     <script type="text/javascript">
         window.location = "/welldonepage.php";
     </script>
     <?php
-}
+}*/
