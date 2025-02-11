27f498ef19c2278144c104cde59a43e8833927d2
Mario <you@example.com>
2025-02-11 12:01:57 +0300

    test

--- contacts.php
+++ contacts.php
@@ -1,11 +1,11 @@
 <?php
-	$description ='Данные для связи';
-	$title = 'Контакты';
-	include 'src/meta.php';
-	include 'src/header.php';
-	
-	
-    
+$description = 'Данные для связи';
+$title = 'Контакты';
+include 'src/meta.php';
+include 'src/header.php';
+
+
+
 
 echo '
 <div class="px-4 py-5 my-5 text-center">
@@ -14,6 +14,4 @@ echo '
     <h1 class="display-5 fw-bold">m6132@yandex.ru</h1>
 	<p class="lead mb-4">Вы можете позвонить или написать нам, если у Вас появились вопросы</p>
 </div>';
-
-
-	include 'src/footer.php';
+include 'src/footer.php';
