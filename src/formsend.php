0c992e160d0b90054ebffa6a0eb8544c3d32598e
Mario <you@example.com>
2025-02-11 14:12:51 +0300

    test

--- src/formsend.php
+++ src/formsend.php
@@ -1,20 +1,21 @@
 <?php
 $name = $_POST['name'];
 $phone = $_POST['phone'];
-$chat_name = "@MarkAndNastya";
+//$chat_name = "@MarkAndNastya";
+$chat_name = "@sfrlaw";
 $token = "7471342210:AAEDkhuLXZootfnjOjDWpbKoeNLSuxzJhUw";
 $message = "Привет. \nУ нас заявочка с сайта расчет пенсии.\nИмя клиента - $name\n телефон - $phone\n Всем хорошего дня!";
 
 $text = urlencode($message);
-//$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id=sfrlaw&text={$text}";
+$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_name}&text={$text}";
 
-$url = "https://api.telegram.org/bot7471342210:AAEDkhuLXZootfnjOjDWpbKoeNLSuxzJhUw/sendMessage?chat_id=markandNastya&text=test";
-echo"$url";
+//$url = "https://api.telegram.org/bot7471342210:AAEDkhuLXZootfnjOjDWpbKoeNLSuxzJhUw/sendMessage?chat_id=@sfrlaw&text=test";
+
 $ch = curl_init();
 $optArray = [CURLOPT_URL => $url, CURLOPT_RETURNTRANSFER => true];
 curl_setopt_array($ch, $optArray);
 $result = curl_exec($ch);
-echo"$result";
+
 curl_close($ch);
 
 /*
